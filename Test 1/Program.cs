c63846852400bc3820177ff91f9a28dde1576021
mopa01 <111962656+Dungeater1337@users.noreply.github.com>
2023-02-17 10:21:41 +0100

    Add project files.

---
+++ Test 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Test1
+{
+    class progeam
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ner två heltal! (bredd och höjd)");
+            int heltal1 = int.Parse(Console.ReadLine());
+            int heltal2 = int.Parse(Console.ReadLine());
+            AREA (heltal1, heltal2);
+            Console.WriteLine($"Arean blir:{heltal1 * heltal2} cm^2");
+
+
+        }
+
+        static int AREA(int heltal1, int heltal2)
+        {
+            return (heltal1 * heltal2);
+        }
+    }
+}
